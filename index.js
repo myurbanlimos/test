59fbb9e01584782f185e3d2a30902be717eb6fa2
myurbanlimos <myurbanlimos@gmail.com>
2025-11-07 23:13:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ app.get("/", (req,res)=>{
     res.send("<h1>Hello Word This is Work</h1>");
 });
 
-app.listen(3000, ()=>{
-    console.log("Port Run 3000");
-    
-})
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
