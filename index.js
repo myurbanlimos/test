07adc20d277fa61a773e8f90897060068c6daeae
myurbanlimos <myurbanlimos@gmail.com>
2025-11-07 23:17:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 const app = express();
 
-const PORT = 3000;
+
 
 app.get("/", (req,res)=>{
     res.send("<h1>Hello Word This is Work</h1>");
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Server running on port ${PORT}`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${3000}`);
 });
